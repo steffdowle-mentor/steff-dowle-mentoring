281865e160bfb09ab9eea99e8b3ea627dfdbb558
steffdowle-mentor <steffdowle@outlook.com>
2026-06-20 09:49:43 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -86,6 +86,9 @@ function App() {
     </p>
   </div>
 </section>
+        <div id="soro-blog"></div>
+<script src="https://app.trysoro.com/api/embed/b553c465-9609-4d47-87c9-ecd1c0561729" defer></script>
+
         <section className="section light">
   <p className="eyebrow dark">Testimonials</p>
 
